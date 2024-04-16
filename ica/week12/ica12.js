c8475864b104a537e3cf319f02d5acac2d4c4f3b
azrazvi <aira8419@colorado.edu>
2024-04-15 22:04:33 -0600

    wa12

--- ica/week12/ica12.js
+++ ica/week12/ica12.js
@@ -5,7 +5,7 @@ const answerbtn = document.querySelector("#js-tweet");
 answerbtn.addEventListener('click', getAnswer);
 const answerText = document.querySelector("#js-answer-text");
 
-const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+const endpoint = 'https://api.quotable.io/random'
 
 let answer = '';
 
@@ -17,10 +17,10 @@ async function getQuote() {
                 throw Error(response.statusText)
             }
             const json = await response.json();
-            console.log(json['question']);
-            displayQuote(json['question']);
-            console.log(json['answer']);
-            answer = json['answer'];
+            console.log(json['content']);
+            displayQuote(json['content']);
+            console.log(json['author']);
+            answer = json['author'];
             answerText.textContent = '';
     } catch (err) {
         console.log(err);
