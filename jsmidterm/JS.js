82d01a6198d761b0770b7eec305337aadd866cf8
azrazvi <aira8419@colorado.edu>
2024-05-05 22:49:34 -0600

    final

--- jsmidterm/JS.js
+++ jsmidterm/JS.js
@@ -26,9 +26,34 @@ const button1 = document.querySelector("button1");
 
 
           document.getElementById('inc').value = newValue;
+          var sound = document.getElementById('audio1');
+          sound.play();
+  
+        }
 
-    
-    
+        function buttonClick1() {
+          let currentValue = 0;
+
+          const increment = random(-99,0);
+
+          let newValue = currentValue + increment;
+
+
+          document.getElementById('inc').value = newValue;
+          var sound = document.getElementById('audio');
+          sound.play();
+  
+        }
+
+        function buttonClick2(){
+          let currentValue = 0
+
+          const increment = random(0,1)
+
+          let newValue = currentValue + increment;
+
+
+          document.getElementById('inc').value = newValue;
   
         }
 
